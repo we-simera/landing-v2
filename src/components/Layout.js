17a08375d7524a76bc3e8d7a524da90299565ba7
Joshuafrankle <joshuafrankle7@gmail.com>
2022-03-20 18:00:15 +0530

    removed navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,10 @@
 import React from "react"
 import "../assets/css/style.css"
-import NavBar from "./NavBar"
 import Footer from "./Footer"
 
 export default function Layout({ children }) {
   return (
     <>
-      <NavBar />
       <section className="container-fluid">{children}</section>
       <Footer />
     </>
