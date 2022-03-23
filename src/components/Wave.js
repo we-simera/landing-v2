d7d16bbbaefa4865f6d9e4c25eaf1aa78d97e178
Joshuafrankle <joshuafrankle7@gmail.com>
2022-03-24 02:13:43 +0530

    added second wave

--- src/components/Wave.js
+++ src/components/Wave.js
@@ -3,10 +3,17 @@ import "assets/css/wave.css"
 
 export default function Wave() {
   return (
-    <section className="wave-section d-none d-xl-block">
-      <div className="wave wave2"></div>
-      <div className="wave wave3"></div>
-      <div className="wave wave4"></div>
-    </section>
+    <>
+      <section className="wave-section d-none d-xl-block">
+        <div className="wave wave2"></div>
+        <div className="wave wave3"></div>
+        <div className="wave wave4"></div>
+      </section>
+      <section className="wave-section-2 d-none d-xl-block">
+        <div className="wave-2 wave2"></div>
+        <div className="wave-2 wave3"></div>
+        <div className="wave-2 wave4"></div>
+      </section>
+    </>
   )
 }
